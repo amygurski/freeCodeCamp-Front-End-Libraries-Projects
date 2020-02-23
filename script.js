4bd6f262165c102cc940b73b4373123a1b63212b
Amy Gurski <49253356+amygurski@users.noreply.github.com>
2020-02-23 16:38:58 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,125 @@
+$(document).ready(function() {
+  
+  //Literary Quotes, source: https://bookroo.com/
+  const quote = [
+    {text: "A reader lives a thousand lives before he dies . . . The man who never reads lives only one.", author: ' George R.R. Martin'}, 
+    {text: "Until I feared I would lose it, I never loved to read. One does not love breathing.", author: ' Harper Lee'}, 
+    {text: "Never trust anyone who has not brought a book with them.", author: ' Lemony Snicket'}, 
+    {text: "You can never get a cup of tea large enough or a book long enough to suit me.", author: ' C.S. Lewis'}, 
+    {text: "Reading is essential for those who seek to rise above the ordinary.", author: ' Jim Rohn'}, 
+    {text: "I find television very educating. Every time somebody turns on the set, I go into the other room and read a book.", author: ' Groucho Marx'}, 
+    {text: "‘Classic’ -  a book which people praise and don’t read.", author: ' Mark Twain'}, 
+    {text: "You don’t have to burn books to destroy a culture. Just get people to stop reading them.", author: ' Ray Bradbury'}, 
+    {text: "So please, oh please, we beg, we pray, go throw your TV set away, and in its place you can install a lovely bookshelf on the wall.", author: ' Roald Dahl'}, 
+    {text: "Think before you speak. Read before you think.", author: ' Fran Lebowitz'}, 
+    {text: "Let’s be reasonable and add an eighth day to the week that is devoted exclusively to reading.", author: ' Lena Dunham'}, 
+    {text: "The reading of all good books is like conversation with the finest (people) of the past centuries.", author: ' Descartes'}, 
+    {text: "In the case of good books, the point is not to see how many of them you can get through, but rather how many can get through to you.", author: ' Mortimer J. Adler'}, 
+    {text: "Reading one book is like eating one potato chip.", author: ' Diane Duane'}, 
+    {text: "The more that you read, the more things you will know. The more that you learn, the more places you’ll go.", author: ' Dr. Seuss'}, 
+    {text: "Books are a uniquely portable magic.", author: ' Stephen King'}, 
+    {text: "I read a book one day and my whole life was changed.", author: ' Orhan Pamuk'}, 
+    {text: "People say that life is the thing, but I prefer reading.", author: ' Logan Pearsall Smith'}, 
+    {text: "Today a reader, tomorrow a leader.", author: ' Margaret Fuller'}, 
+    {text: "People can lose their lives in libraries. They ought to be warned.", author: ' Saul Bellow'}, 
+    {text: "A good book is an event in my life.", author: ' Stendhal'}, 
+    {text: "Reading brings us unknown friends", author: ' Honoré de Balzac'}, 
+    {text: "The world was hers for the reading.", author: ' Betty Smith'}, 
+    {text: "I kept always two books in my pocket, one to read, one to write in.", author: ' Robert Louis Stevenson'}, 
+    {text: "The person who deserves most pity is a lonesome one on a rainy day who doesn’t know how to read.", author: ' Benjamin Franklin'}, 
+    {text: "Once you have read a book you care about, some part of it is always with you.", author: ' Louis L’Amour'}, 
+    {text: "Literature is the most agreeable way of ignoring life.", author: ' Fernando Pessoa'}, 
+    {text: "No. I can survive well enough on my own—if given the proper reading material.", author: ' Sarah J. Maas'}, 
+    {text: "There is more treasure in books than in all the pirate's loot on Treasure Island.", author: ' Walt Disney'}, 
+    {text: "We are of opinion that instead of letting books grow moldy behind an iron grating, far from the vulgar gaze, it is better to let them wear out by being read.", author: ' Jules Verne'}, 
+    {text: "My alma mater was books, a good library…. I could spend the rest of my life reading, just satisfying my curiosity.", author: ' Malcolm X'}, 
+    {text: "It’s not that I don’t like people. It’s just that when I’m in the company of others -  even my nearest and dearest -  there always comes a moment when I’d rather be reading a book.", author: ' Maureen Corrigan'}, 
+    {text: "Books to the ceiling, Books to the sky, My pile of books is a mile high. How I love them! How I need them! I’ll have a long beard by the time I read them.", author: ' Arnold Lobel'}, 
+    {text: "There is nothing more luxurious than eating while you read—unless it be reading while you eat.", author: ' E. Nesbit'}, 
+    {text: "One glance at a book and you hear the voice of another person, perhaps someone dead for 1,000 years. To read is to voyage through time.", author: ' Carl Sagan'}, 
+    {text: "I have a passion for teaching kids to become readers, to become comfortable with a book, not daunted. Books shouldn’t be daunting, they should be funny, exciting and wonderful; and learning to be a reader gives a terrific advantage.", author: ' Roald Dahl'}, 
+    {text: "Man reading should be man intensely alive. The book should be a ball of light in one’s hand.", author: ' Ezra Pound'}, 
+    {text: "If we encounter a man of rare intellect, we should ask him what books he reads.", author: ' Ralph Waldo Emerson'}, 
+    {text: "Always read something that will make you look good if you die in the middle of it.", author: ' P.J. O’Rourke'}, 
+    {text: "Many people, myself among them, feel better at the mere sight of a book.", author: ' Jane Smiley'}, 
+    {text: "Beware of the person of one book.", author: ' Thomas Aquinas'}, 
+    {text: "Read the best books first, or you may not have a chance to read them at all.", author: ' Henry David Thoreau'}, 
+    {text: "Make it a rule never to give a child a book you would not read yourself.", author: ' George Bernard Shaw'}, 
+    {text: "Books serve to show a man that those original thoughts of his aren’t very new after all.", author: ' Abraham Lincoln'}, 
+    {text: "I can’t imagine a man really enjoying a book and reading it only once.", author: ' C.S. Lewis'}, 
+    {text: "You know you’ve read a good book when you turn the last page and feel a little as if you have lost a friend.", author: ' Paul Sweeney'}, 
+    {text: "Some books leave us free and some books make us free.", author: ' Ralph Waldo Emerson'}, 
+    {text: "Once you learn to read, you will be forever free.", author: ' Frederick Douglas'}, 
+    {text: "There are many little ways to enlarge your child’s world. Love of books is the best of all.", author: ' Jacqueline Kennedy Onassis'}, 
+    {text: "Wear the old coat and buy the new book.", author: ' Austin Phelps'}, 
+    {text: "You may have tangible wealth untold. Caskets of jewels and coffers of gold. Richer than I you can never be — I had a mother who read to me.", author: ' Strickland Gillilan'}, 
+    {text: "The man who does not read good books is no better than the man who can’t.", author: ' Mark Twain'}, 
+    {text: "To learn to read is to light a fire; every syllable that is spelled out is a spark.", author: ' Victor Hugo'}, 
+    {text: "Oh, magic hour, when a child first knows she can read printed words!", author: ' Betty Smith'}, 
+    {text: "A book is a gift you can open again and again.", author: ' Garrison Keillor'}, 
+    {text: "Fill your house with stacks of books, in all the crannies and all the nooks.", author: ' Dr. Seuss'}, 
+    {text: "Show me a family of readers, and I will show you the people who move the world.", author: ' Napoléon Bonaparte'}, 
+    {text: "A classic is a book that has never finished saying what it has to say.", author: ' Italo Calvino'}, 
+    {text: "I have always imagined that paradise will be a kind of library.", author: ' Jorge Luis Borges'}, 
+    {text: "Reading is a discount ticket to everywhere.", author: ' Mary Schmich'}, 
+    {text: "No entertainment is so cheap as reading, nor any pleasure so lasting.", author: ' Mary Wortley Montagu'}, 
+    {text: "If you are going to get anywhere in life you have to read a lot of books.", author: ' Roald Dahl'}, 
+    {text: "That perfect tranquility of life, which is nowhere to be found but in retreat, a faithful friend and a good library.", author: ' Aphra Behn'}, 
+    {text: "To acquire the habit of reading is to construct for yourself a refuge from almost all the miseries of life.", author: ' W. Somerset Maugham'}, 
+    {text: "Rainy days should be spent at home with a cup of tea and a good book.", author: ' Bill Watterson'}, 
+    {text: "These books gave Matilda a hopeful and comforting message: You are not alone.", author: ' Roald Dahl'}, 
+    {text: "I guess there are never enough books.", author: ' John Steinbeck'}, 
+    {text: "Despite the enormous quantity of books, how few people read! And if one reads profitably, one would realize how much stupid stuff the vulgar herd is content to swallow every day.", author: ' Voltaire'}, 
+    {text: "If you don’t like to read, you haven’t found the right book.", author: ' J.K. Rowling'}, 
+    {text: "Libraries will get you through times of no money better than money will get you through times of no libraries.", author: ' Anne Herbert'}, 
+    {text: "Ah, how good it is to be among people who are reading.", author: ' Rainer Maria Rilke'}, 
+    {text: "Children are made readers on the laps of their parents.", author: ' Emilie Buchwald'}, 
+    {text: "I couldn’t live a week without a private library -  indeed, I’d part with all my furniture and squat and sleep on the floor before I’d let go of the 1500 or so books I possess.", author: ' H.P. Lovecraft'}, 
+    {text: "Books are good company, in sad times and happy times, for books are people -  people who have managed to stay alive by hiding between the covers of a book.", author: ' E.B. White'}, 
+    {text: "Luckily, I always travel with a book, just in case I have to wait on line for Santa, or some such inconvenience.", author: ' David Levithan'}, 
+    {text: "Outside of a dog, a book is a man’s best friend. Inside of a dog, it’s too dark to read.", author: ' Groucho Marx'}, 
+    {text: "I always read. You know how sharks have to keep swimming or they die? I’m like that. If I stop reading, I die.", author: ' Patrick Rothfuss'}, 
+    {text: "There is no Frigate like a Book To take us Lands away", author: ' Emily Dickinson'}, 
+    {text: "I intend to put up with nothing that I can put down.", author: ' Edgar Allan Poe'}, 
+    {text: "Books are not made for furniture, but there is nothing else that so beautifully furnishes a house.", author: ' Henry Ward Beecher'}, 
+    {text: "I took a speed reading course and read War and Peace in twenty minutes. It involves Russia.", author: ' Woody Allen'}, 
+    {text: "There are worse crimes than burning books. One of them is not reading them.", author: ' Joseph Brodsky'}, 
+    {text: "Books break the shackles of time  proof that humans can work magic.", author: ' Carl Sagan'}, 
+    {text: "Employ your time in improving yourself by other men’s writings so that you shall come easily by what others have labored hard for.", author: ' Socrates'}, 
+    {text: "For my whole life, my favorite activity was reading. It’s not the most social pastime.", author: ' Audrey Hepburn'}, 
+    {text: "Reading for me, is spending time with a friend.", author: ' Gary Paulsen'}, 
+    {text: "[F]rom the reading of ‘good books’ there comes a richness of life that can be obtained in no other way.", author: ' Gordon B. Hinckley'}, 
+    {text: "Fools have a habit of believing that everything written by a famous author is admirable. For my part I read only to please myself and like only what suits my taste.", author: ' Voltaire'}, 
+    {text: "How many a man has dated a new era in his life from the reading of a book.", author: ' Henry David Thoreau'}, 
+    {text: "Reading is a basic tool in the living of a good life.", author: ' Mortimer J. Adler'}, 
+    {text: "A children’s story that can only be enjoyed by children is not a good children’s story in the slightest.", author: ' C.S. Lewis'}, 
+    {text: "It is a great thing to start life with a small number of really good books which are your very own.", author: ' Arthur Conan Doyle'}, 
+    {text: "Finally, from so little sleeping and so much reading, his brain dried up and he went completely out of his mind.", author: ' Miguel de Cervantes Saavedra'}, 
+    {text: "What better occupation, really, than to spend the evening at the fireside with a book, with the wind beating on the windows and the lamp burning bright.", author: ' Gustave Flaubert'}, 
+    {text: "I’m old fashioned and think that reading books is the most glorious pastime that humankind has yet devised.", author: ' Wisława Szymborska'}, 
+    {text: "Never put off till tomorrow the book you can read today.", author: ' Holbrook Jackson'}, 
+    {text: "What kind of life can you have in a house without books?", author: ' Sherman Alexie'}, 
+    {text: "Of course anyone who truly loves books buys more of them than he or she can hope to read in one fleeting lifetime. A good book, resting unopened in its slot on a shelf, full of majestic potentiality, is the most comforting sort of intellectual wallpaper.", author: ' David Quammen'}
+    ];
+  
+  var currentQuote = $("#text").text(quote[0].text);
+  var currentAuthor = $("#author").text(" - " + quote[0].author);
+  
+  //When refresh clicked, choose new random quote.
+  $("#new-quote").on("click", function() {
+    var r = getRandomNumber();  
+    currentQuote = quote[r].text;
+    currentAuthor = " - " + quote[r].author;
+    $("#text").text(currentQuote);
+    $("#author").text(currentAuthor);
+     });
+  
+  //Tweet Quote
+  $("#tweet-quote").on("click", function() {
+    $("#tweet-quote").attr("href", "http://twitter.com/intent/tweet?text=" + currentQuote + currentAuthor + "&via=amygurski4" + "&hashtags=booklover");
+  });
+  
+  function getRandomNumber() {
+    return Math.floor(Math.random() * quote.length);
+  }
+});
